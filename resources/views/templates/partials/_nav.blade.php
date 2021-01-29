faafd38038ca4cdd6b4dcd9aca5636f68fab6bdc
Morgan <gann.gann87@gmail.com>
2021-01-29 20:37:39 +0100

    Menu button -> classe active

--- resources/views/templates/partials/_nav.blade.php
+++ resources/views/templates/partials/_nav.blade.php
@@ -7,16 +7,18 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="Basica"></a>
+            <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('assets') }}/img/logo.png" alt="Basica"></a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="#TODO active"><a href="{{ route('home') }}">Home</a></li>
-                <li><a href="{{ route('works.index') }}">Portfolio</a></li>
-                <li><a href="{{ route('posts.index') }}">Blog</a></li>
-                <li><a href="{{ route('pages.contact') }}">Contact</a></li>
+                <li class="{{ Route::currentRouteName() == 'home' ? 'active' : '' }}"><a href="{{ route('home') }}">Home</a></li>
+                <li class="{{ Route::currentRouteName() == 'works.index' ? 'active' : '' }}"><a href="{{ route('works.index') }}">Portfolio</a></li>
+                <li class="{{ Route::currentRouteName() == 'posts.index' ? 'active' : '' }}"><a href="{{ route('posts.index') }}">Blog</a></li>
+                <li class="{{ Route::currentRouteName() == 'pages.contact' ? 'active' : '' }}"><a href="{{ route('pages.contact') }}">Contact</a></li>
             </ul>
         </div>
     </div>
 </header>
 <!--/header-->
+
+
